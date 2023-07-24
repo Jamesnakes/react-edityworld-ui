df7b9836aed9441d885f9179a69a12c6e97fcf71
gaolulu <gaolulu@edityworld.com>
2023-07-24 15:29:48 +0800

    代码提交

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,6 +1,7 @@
 import http from '../config/axios/index'
 
 function getList() {
+
     return new Promise((resolve,reject) => {
         http("post", '/article/home/index').then(res => {
             resolve(res)
